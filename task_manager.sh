6452260204ece1af9ae9b0ab32d0830c1c0fd0a5
Rom-Code <4husam@gmail.com>
2025-06-09 11:45:40 +0000

    list task

--- task_manager.sh
+++ task_manager.sh
@@ -6,3 +6,16 @@ create_task_file(){
 	echo "Task file created."
 	fi
 }
+
+add_task(){
+	read -p "Enter Task description: " desc
+	read -p "Enter due date (YYYY-MM-DD): " due
+	echo "o}$due}$desc" >> tasks.txt
+	echo "Task added."
+}
+
+list_task(){
+	echo "Task:"
+	sort -t"|" -k2 tasks.txt | awk -F"|| '{ status=($1=="1") ? "Done" : "Pending;
+print NR ". [" status "] " $$3 " (Due: " $2 ")" }'
+}
